ad9fbf0d6da90a058e4603c80dc8b8009b6e987b
Audrea Cook <audreacwork@gmail.com>
2019-10-15 13:02:47 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+let main = document.getElementById("main");
+main.remove();
+
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+document.querySelector('h1').id = 'victory';
+
+document.querySelector('h1#victory').innerHTML = 'AUDREA is the champion';
