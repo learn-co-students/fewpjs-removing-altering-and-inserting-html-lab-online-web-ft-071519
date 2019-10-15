6aa6a3545c45a4b3f132b861747181a55fb74194
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-15 14:26:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById("main").remove();
+let newHeader = document.createElement('h1');
+newHeader.className = "victory";
+newHeader.id="victory"
+newHeader.innerHTML = "is the champion!";
+document.head.querySelector("title").appendChild(newHeader);
