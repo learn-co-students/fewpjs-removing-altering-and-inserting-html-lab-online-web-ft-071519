a199b2e120dfdd34f5d48c1cb9e92e5401322b8e
radville <laura.radville@gmail.com>
2019-10-24 20:31:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+let newHeader = document.createElement('h1')
+newHeader.setAttribute("id", "victory");
+
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = 'Laura is the champion';
+
+const element = document.querySelector("main");
+
+element.remove();
